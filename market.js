4d89f6361acadf745e08f11f1069ea66ed4d5eb3
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-10-13 01:03:53 -0300

    Create market.js

---
+++ market.js
@@ -0,0 +1,9 @@
+    var text_body = document.getElementById("text-body");
+    var cad_client = document.getElementById("client");
+    cad_client.addEventListener("click", function () {
+        text_body.innerHTML = "OLHA LÁ!";
+        text_body.style.visibility = "visible";
+    })
+
+
+
