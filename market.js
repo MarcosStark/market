47cf3818ce822c6af282aaa1ae243cae10f43fef
MarcosStark <88205157+MarcosStark@users.noreply.github.com>
2023-10-13 03:24:07 -0300

    #3

--- market.js
+++ market.js
@@ -1,10 +1,21 @@
     var text_body = document.getElementById("text-body");
     var clientReg_btn = document.getElementById("clientReg-btn");
+    var productReg_btn = document.getElementById("productReg-btn");
     var client_reg = document.getElementById("client-reg");
+    var product_reg = document.getElementById("product-reg");
 
     clientReg_btn.addEventListener("click", function () {
         text_body.style.backgroundImage = "url('')";
+        product_reg.style.display = "none";
+        client_reg.style.display = "";
         client_reg.style.visibility = "visible";
+    })
+
+    productReg_btn.addEventListener("click", function () {
+        text_body.style.backgroundImage = "url('')";
+        client_reg.style.display = "none";
+        product_reg.style.display = "";
+        product_reg.style.visibility = "visible";
     })
 
 
