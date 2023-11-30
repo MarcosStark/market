07c62aaf2d57e7b82f638c57f5718e0ff8c3e79b
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2023-11-30 19:56:08 -0300

    #include Ajad&Jquery

---
+++ market_ajax&jquery.js
@@ -0,0 +1,30 @@
+text_body = document.getElementById("text-body");
+
+$(document).ready( function(){
+    
+    $('#clientReg-btn').click(function(){
+        $.ajax({
+            url: 'client_reg.html',
+
+            success: function(data){
+                text_body.style.backgroundImage = "url('')";
+                $('#text-body').html(data);
+            }
+        })
+    });
+});
+
+$(document).ready( function(){
+    
+    $('#productReg-btn').click(function(){
+        $.ajax({
+            url: 'product_reg.html',
+
+            success: function(data){
+                text_body.style.backgroundImage = "url('')";
+                $('#text-body').html(data);
+            }
+        })
+    });
+});
+
