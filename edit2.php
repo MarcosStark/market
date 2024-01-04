7c123da7db65cc02ac29559a9862093e19ef2c35
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2024-01-03 22:23:08 -0300

    Update edit2.php

--- edit2.php
+++ edit2.php
@@ -2,16 +2,15 @@
 
     require_once('db_connection.php');
 
-    $sql = "SELECT * FROM client ORDER BY id DESC";
-
     $objDb = new db();
     $link = $objDb->mysql_connection();
-    
+
+    $sql = "SELECT * FROM client ORDER BY id DESC";
+
     $result = mysqli_query($link, $sql);
 
-
-   if($result){
-
+    $rows = mysqli_fetch_array($result);
+ 
         echo "<tr>";
    
         echo "<th style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"; echo "ID"; "</th>";
@@ -21,24 +20,24 @@
         echo "<th style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"; echo "email"; "</th>";
         echo "<th style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"; echo "Telefone"; "</th>";
         echo "</tr>";
-        
-        while($rows = mysqli_fetch_array($result)){
+
+   
+     
             echo "<tr>";
                 echo "<a href='#' class='list-group-itemtext-align: center;'>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>".$rows['id']."</td>";
                 echo "<form id='client-edit'>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input class='inputEdit_client' name='name' value=$rows[name]>"."</td>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input class='inputEdit_client' name='age' value=$rows[age]>"."</td>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input class='inputEdit_client' name='cpf' value=$rows[cpf]>"."</td>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input class='inputEdit_client' name='email' value=$rows[email]>"."</td>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input class='inputEdit_client' name='telephone' value=$rows[telephone]>"."</td>";
-                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<button class='edit2-btn' type='submit' data-id_client='".$rows['id']."'>"."Salvar"."</button>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>".$rows['id']."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input type='text' name='name' value='$rows[name]'>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input type='text' name='age' value='$rows[age]'>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input type='text' name='cpf' value='$rows[cpf]'>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input type='email' name='email' value='$rows[email]'>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<input type='text' name='telephone' value='$rows[telephone]'>"."</td>";
+                echo "<td style='border: 3px solid black; border-style: inset; padding: 3px; text-align: center;'>"."<button class='edit2-btn' type='button' data-id_client='$rows[id]'>"."Salvar"."</button>"."</td>";
                 echo "</form>";
+
                 echo "</a>";
             echo "</tr>";
-        }
-
-    }
+        
 
     echo "<script src='market_bd.js'></script>";
 ?>
